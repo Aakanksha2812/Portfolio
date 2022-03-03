d58f111e22dc780fcf444638d1bbb176da54ce68
Aakanksha <shindeaakanksha28@gmail.com>
2022-03-03 12:26:29 +0530

    about page updated

--- src/Components/about.js
+++ src/Components/about.js
@@ -1,36 +1,32 @@
+import { Button } from 'bootstrap';
 import React from 'react'
 import img from '../Assets/m3.png'
 import './about.css'
-function about () {
+function about() {
     return (
         <div className='a'>
             <div className='a-left'>
                 <div className='a-card'>
-           <img 
-           src={img}
-           className='a-img'
-           />
+                    <img
+                        src={img}
+                        className='a-img'
+                    />
                 </div>
             </div>
             <div className='a-right'>
                 <div className='a-title'>
-         <h1>About me</h1>
+                    <h1>About me</h1>
                 </div>
                 <div className='a-text'>
-               <p>   My name is Aakanksha Shinde. I am Enginnering Student. <br/> i am doing engineering from
-                   Aissms Ioit. I am web developer. <br/>i am working on HTML,CSS, Javascript,React,  nodejs,Mysql.<br/>
-                   apart from that i like reading,cooking,travelling
-               </p>
-               <h2 className='resume'> you will find my resume here</h2>
-               <div className="resume">
-              <a href="https://drive.google.com/file/d/1-yaTG0X5ntmcQEb9I_zGAbj4Sfgwaaok/view?usp=sharing">
-                <button className="btn btn-primary"> MY Resume</button>
-              </a>
-            </div>
-               
-            </div>
-            
-</div>        </div>
+                    <p>   My name is Aakanksha Shinde. I am Enginnering Student. <br /> i am doing engineering from
+                        Aissms Ioit. I am web developer. <br />i am working on HTML,CSS, Javascript,React,  nodejs,Mysql.<br />
+                        apart from that i like reading,cooking,travelling
+                      
+                    </p>
+                    <Button>resume</Button>
+                </div>
+
+            </div>        </div>
     )
 }
 export default about;
